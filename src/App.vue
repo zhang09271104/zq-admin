2b6ac9fe010c40342c6e4c61d0b4f9eab87a7f09
zhang <2395685843@qq.com>
2026-01-03 14:34:36 +0800

    feat: 更新src文件

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <router-view />
+    <!--    <Home/>-->
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup lang="ts">
+// import Home from "@views/Home.vue";
+// 可以在这里添加全局逻辑
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#app {
+  width: 100%;
+  min-height: 100vh;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
+import router from './router/index.ts'
+import '@/styles/element-variables.scss'
+import '@/styles/index.scss'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
+app.mount('#app')
