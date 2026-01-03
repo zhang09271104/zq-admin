3057ec81c5591845861dd497a64d9e801ec63c20
zhang <2395685843@qq.com>
2026-01-03 14:33:40 +0800

    feat: 添加views文件

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>欢迎使用 Vue 3 + TypeScript + Vite---{{ message }}</h1>
+    <router-link to="/about">关于</router-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('Hello Vue 3!')
+</script>
+
+<style scoped>
+.home {
+  padding: 20px;
+}
+</style>
