fb81da6d6cc03191f1d2648376e59d5e8f8e8aff
zhang <2395685843@qq.com>
2026-01-31 11:01:19 +0800

    feat: 登录的模块表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,13 +6,13 @@
         11222
       </el-col>
       <el-col :span="12" :xs="24">
-        <el-form :model="loginForm" class="login_form">
+        <el-form ref="loginFormRef" class="login_form" :model="loginForm" :rules="rules">
           <h1>Hello</h1>
           <h2>欢迎来到zq后台管理系统</h2>
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input v-model="loginForm.username" :prefix-icon="User" />
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               v-model="loginForm.password"
               type="password"
@@ -42,17 +42,52 @@ import { getTime } from '@utils/time.ts'
 import useUserStore from '@store/modules/user.ts'
 const userStore = useUserStore()
 
+//获取表单实例
+const loginFormRef = ref()
+
 const $router = useRouter()
 //收集账号和密码数据
 const loginForm = reactive({
   username: 'admin',
   password: '111111'
 })
+//定义表单校验需要配置对象
+// const rules={
+//   username: [
+//     { required: true, min: 5, max: 10, message: '账号长度至少是6位', trigger: 'blur' },
+//   ],
+//   password: [
+//     { required: true, min: 6, max: 15, message: '账号长度至少是6位', trigger: 'change' },
+//   ],
+// }
+//自定义校验规则
+const validatePassword = (rule: any, value: any, callback: any) => {
+  if (value.length >= 6) {
+    callback()
+  } else {
+    callback(new Error('密码长度至少是6位'))
+  }
+}
+
+const validateUsername = (rule: any, value: any, callback: any) => {
+  if (value.length >= 5) {
+    callback()
+  } else {
+    callback(new Error('账号长度至少是5位'))
+  }
+}
+const rules = {
+  username: [{ validator: validateUsername, trigger: 'blur' }],
+  password: [{ validator: validatePassword, trigger: 'change' }]
+}
+
 //定义登录按钮加载的效果
 const loading = ref(false)
 //登录按钮的回调.
 const login = async () => {
   loading.value = true
+
+  await loginFormRef.value.validate()
   try {
     //保证登陆成功
     await userStore.userLogin(loginForm)
