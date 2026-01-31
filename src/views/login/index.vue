881e258d8544ba3e9950d228a7467389506ddb9c
zhang <2395685843@qq.com>
2026-01-31 09:49:59 +0800

    feat: 登录的当前时间的判断与封装

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -36,6 +36,8 @@ import { User, Lock } from '@element-plus/icons-vue'
 import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElNotification } from 'element-plus'
+//引入获取当前时间的函数
+import { getTime } from '@utils/time.ts'
 //引入用户相关的小仓库
 import useUserStore from '@store/modules/user.ts'
 const userStore = useUserStore()
@@ -58,7 +60,9 @@ const login = async () => {
     await $router.push('./')
     ElNotification({
       type: 'success',
-      message: '登录成功'
+      // title:'Hi,'+getTime()+'好！',
+      title: `Hi,${getTime()}好！`,
+      message: '欢迎回来'
     })
     loading.value = false
   } catch (error) {
