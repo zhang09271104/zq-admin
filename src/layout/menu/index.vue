d549112f060bea1882cfc5b69c774b79a0808ea0
zhang <2395685843@qq.com>
2026-02-02 19:24:50 +0800

    feat: 递归组件生成动态菜单开发

--- src/layout/menu/index.vue
+++ src/layout/menu/index.vue
@@ -2,21 +2,38 @@
   <div>
     <template v-for="item in menuList" :key="item.path">
       <template v-if="!item.children">
-        <el-menu-item v-if="!item.meta.hidden">
+        <el-menu-item v-if="!item.meta.hidden" :index="item.path" @click="goRoute">
           <template #title>
+            <el-icon>
+              <component :is="item.meta.icon" />
+            </el-icon>
             {{ item.meta.title }}
           </template>
         </el-menu-item>
       </template>
       <template v-if="item.children && item.children.length === 1">
-        <el-menu-item v-if="!item.children[0].meta.hidden">
+        <el-menu-item
+          v-if="!item.children[0].meta.hidden"
+          :index="item.children[0].path"
+          @click="goRoute"
+        >
           <template #title>
+            <el-icon>
+              <component :is="item.children[0].meta.icon" />
+            </el-icon>
             {{ item.children[0].meta.title }}
           </template>
         </el-menu-item>
       </template>
-      <el-sub-menu v-if="item.children && item.children.length > 1" index="2-4">
+      <el-sub-menu
+        v-if="item.children && item.children.length > 1"
+        :index="item.path"
+        @click="goRoute"
+      >
         <template #title>
+          <el-icon>
+            <component :is="item.meta.icon" />
+          </el-icon>
           {{ item.meta.title }}
         </template>
         <NavMenu :menu-list="item.children" />
@@ -29,11 +46,10 @@ defineOptions({
   name: 'NavMenu'
 })
 defineProps(['menuList'])
+
+const goRoute = (value: any) => {
+  console.log(value)
+}
 </script>
-<!--<script lang="ts">-->
-<!--export default {-->
-<!--  name: 'Menu',-->
-<!--}-->
-<!--</script>-->
 
 <style scoped></style>
